95a8d5aec5826188001529db58b24e2a7bcc982d
hantnTLU <54317348+hantnTLU@users.noreply.github.com>
2020-08-13 16:11:20 +0200

    Add files via upload

---
+++ script_infer_signature_gene_level_relapse.R
@@ -0,0 +1,193 @@
+################
+# Author : Thi Ngoc Ha Nguyen
+# Date   : 05/07/2020
+# Email  : thi-ngoc-ha.nguyen@i2bc.paris-saclay.fr
+################
+
+rm(list=ls())
+set.seed(12678)
+
+################
+# Load libraries
+################
+library(data.table)
+################
+# Load list of functions 
+################
+source("useful_functions.R")
+#######################################################################
+## scripts
+#######################################################################
+
+# Discovery data using top 100 NB10 gene and contig
+
+topGene <-"Data_discovery/top2k_TCGA_gene_norm.nb5.out"
+sampleTCGA <-"Data_discovery/sample_conditions.tsv"
+
+# Validation ICGC data
+sampleICGC <- "Data_validation/sample_cond_icgc.tsv"
+geneICGC <- "Data_validation/gene-norm-ICGC-Relapse-2-2.tsv"
+
+# Validation Stello data
+sampleStelloo <- "Data_validation/sample_cond_stelloo.tsv"
+geneStelloo <- "Data_validation/gene-filter-norm-Stello-Relapse-3-5.tsv"
+
+NUM_RUNS=100
+nKeep = 100
+
+# Directory to store result
+dir.store <- paste0("Result_article/gene_level/", nKeep)
+dir.create(file.path(dir.store), showWarnings = FALSE, recursive = TRUE)
+############################################################################################
+evaluate_gene <- function( sigGeneDis, dataSigDis, dataValidPath, samplesConditionValidPath){
+  
+  # dataframe of signature in validation dataset
+  dataValid <- as.data.frame(fread(dataValidPath, sep="\t", header = TRUE))
+  dataValid <- data.frame(row.names = dataValid$feature, dataValid[,-1], check.names=F)
+  dataSignatureValid <- dataValid[sigGeneDis,]
+  
+  ########## processing of conditions ##############
+  #create sample condition for signature
+  samplesConditionValid<-as.data.frame(fread(samplesConditionValidPath,sep="\t", header= FALSE ,check.names=F))
+  names(samplesConditionValid)<-c("Sample","condition")
+  
+  for (i in 1:dim(samplesConditionValid)[1]){
+    if(samplesConditionValid$condition[i] == "no"){
+      samplesConditionValid$condition[i] = "No"
+    }
+    if(samplesConditionValid$condition[i] == "yes"){
+      samplesConditionValid$condition[i] = "Yes"
+    }
+  }
+  
+  #Transpose
+  dataSignatureValid <- as.data.frame(t(dataSignatureValid))
+  
+  dataSignatureValid$Sample <- row.names(dataSignatureValid)
+  
+  # Mapping count table and condition in validation set
+  dataValid<-merge(dataSignatureValid,samplesConditionValid, 
+                   by.x="Sample",
+                   by.y="Sample",
+                   all.x=TRUE,
+                   all.y=FALSE)
+  dataValid$condition <- factor(dataValid$condition)
+  
+  # dataframe of signature in validation set
+  dataSigValid <- dataValid[,c("condition",sigGeneDis)]  
+  
+  # From gene symbol to gene name
+  signature.idsym <- fromGeneIDtakeGenName(sigGeneDis)
+  signature.save <- signature.idsym$SYMBOL
+  dataSaveValid <- dataValid[,c("Sample", "condition",sigGeneDis)]
+  names(dataSaveValid) <- c("Sample", "condition",signature.save)
+  
+  names(dataSigValid) <- c("condition",signature.save)
+  
+  resSign <- takeDataReturnAUC(frameTrain = dataSigDis, frameTest = dataSigValid, absentContig = c(), status = "relapse")  
+  
+  return(list(resSign, dataSaveValid))
+}
+
+#################
+pipeline <- function(topProbesPath, samplesConditionDisPath, numruns){
+  
+  # loading top  genes/contigs in TCGA discovery dataset based on approach level
+  countTopProbe <- as.data.frame(fread(topProbesPath, sep="\t", header = TRUE))
+  countTopProbe <- data.frame(row.names = countTopProbe$feature, countTopProbe[,-c(1,2)], check.names=F)
+  countTopProbe <- countTopProbe[1:nKeep,]
+
+  ########## processing of conditions ##############
+  samplesConditionDis<-as.data.frame(fread(samplesConditionDisPath,sep="\t", header= FALSE ,check.names=F))
+  names(samplesConditionDis)<-c("Sample","condition")
+  
+  #Transpose
+  countTopProbe <- as.data.frame(t(countTopProbe))
+  
+  countTopProbe$Sample <- row.names(countTopProbe)
+  
+  # Mapping count table and condition in TCGA
+  dataTCGA<-merge(countTopProbe,samplesConditionDis, 
+                  by.x="Sample",
+                  by.y="Sample",
+                  all.x=TRUE,
+                  all.y=FALSE)
+  dataTCGA$condition <- factor(dataTCGA$condition)
+  
+  ########## processing of feature selection ##############
+  
+  # Finding the signature in TCGA discovery dataset
+  dataModel <-dataTCGA[,-which(names(dataTCGA) %in% c("condition","Sample"))]
+  dataModel$condition <-factor(dataTCGA$condition)
+  
+  # Set threshold for Lasso logistic regression
+  th_lasso = 0.5
+  
+  sigTCGA <- extractSignatureStb(dataModel, thres = th_lasso)
+
+  # From gene symbol to gene name
+  signature.idsym <- fromGeneIDtakeGenName(sigTCGA)
+  signature.save <- signature.idsym$SYMBOL
+  
+  print(paste("List", length(signature.save), "genes in signature that found in TCGA:"))
+  print(signature.save)
+  
+  name.file <- paste0(dir.store, "/sig-gene-tcga.txt")
+  
+  write.table(signature.idsym, file = name.file, quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+  # dataframe of signature in TCGA dataset
+  dataSigTCGA <- dataTCGA[,c("condition", sigTCGA)]
+  names(dataSigTCGA) <- c("condition", signature.save)
+
+  name.file <- paste0(dir.store, "/data-sig-gene-tcga.tsv")
+  dataSaveTCGA <- dataTCGA[,c("Sample","condition",sigTCGA)]
+  colnames(dataSaveTCGA) <- c("Sample", "condition", signature.save)
+  write.table(dataSaveTCGA, file = name.file, sep = "\t", quote = FALSE, row.names = FALSE)
+
+  ########## finding signature in validation set ##############
+
+  #Valid in ICGC
+  resSigICGC <- evaluate_gene(dataSigDis = dataSigTCGA, sigGeneDis = sigTCGA, 
+                              dataValidPath = geneICGC, samplesConditionValidPath = sampleICGC) 
+
+  # Store important variable 
+  name.file <- paste0(dir.store, "/import-sig-gene-icgc.tsv")
+  importVar <- as.data.frame(resSigICGC[[1]][[4]]$importance)
+  importVar$Gene <- rownames(resSigICGC[[1]][[4]]$importance)
+  names(importVar) <- c("Overall", "Gene")
+  dataStore <- importVar[round(order(importVar$Overall, decreasing = T),4),]
+  print(dataStore[, c("Gene","Overall")])
+  write.table(dataStore[, c("Gene", "Overall")], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+  #Store data frame valid
+  name.file <- paste0(dir.store, "/data-sig-gene-icgc.tsv")
+  write.table(resSigICGC[[2]], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+  
+  #Valid in Stelloo
+  resSigStelloo <- evaluate_gene(dataSigDis = dataSigTCGA, sigGeneDis = sigTCGA, 
+                                 dataValidPath = geneStelloo, samplesConditionValidPath = sampleStelloo)
+  # Store important variable 
+  name.file <- paste0(dir.store, "/import-sig-gene-stelloo.tsv")
+  importVar <- as.data.frame(resSigStelloo[[1]][[4]]$importance)
+  importVar$Gene <- rownames(resSigStelloo[[1]][[4]]$importance)
+  names(importVar) <- c("Overall", "Gene")
+  dataStore <- importVar[round(order(importVar$Overall, decreasing = T),4),]
+  print(dataStore[, c("Gene","Overall")])
+  write.table(dataStore[, c("Gene", "Overall")], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+  
+  #Store data frame valid
+  name.file <- paste0(dir.store, "/data-sig-gene-stelloo.tsv")
+  write.table(resSigStelloo[[2]], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+  
+  finalTab <- rbind(resSigICGC[[1]][[3]], resSigStelloo[[1]][[3]], resSigICGC[[1]][[1]], resSigICGC[[1]][[2]])
+  rownames(finalTab) <- c("AUC signature gene in ICGC", "AUC signature gene in Stelloo", 
+						  "Mean AUC cross-valid (TCGA)", "SD AUC cross-valid (TCGA)")
+  print(finalTab)
+  
+  name.file <- paste0(dir.store, "/auc-sig-gene.tsv")
+  write.table(finalTab, file = name.file, quote = FALSE, row.name = TRUE, col.names = FALSE)  
+}
+#################################
+#Run the script to infer
+resGene <- pipeline(topProbesPath = topGene, samplesConditionDisPath = sampleTCGA, numruns = NUM_RUNS)
---
+++ script_infer_signature_kmer_level_relapse.R
@@ -0,0 +1,364 @@
+################
+# Author : Thi Ngoc Ha Nguyen
+# Date   : 05/07/2020
+# Email  : thi-ngoc-ha.nguyen@i2bc.paris-saclay.fr
+################
+
+rm(list=ls())
+set.seed(12678)
+
+################
+# Load libraries
+################
+library(data.table)
+library(edgeR)
+################
+# Load list of functions 
+################
+source("useful_functions.R")
+#######################################################################
+## scripts
+#######################################################################
+
+# Discovery data using top 100 NB10 gene and contig
+
+topContig <- "Data_discovery/top2k_contig_merge_norm.nb5.out"
+sampleTCGA <-"Data_discovery/sample_conditions.tsv"
+
+# Validation ICGC data
+sampleICGC <- "/store/EQUIPES/SSFA/MEMBERS/thi-ngoc-ha.nguyen/Prostate_ICGC_148samples/Download/DEkupl_run_Relapse_Mask/Result_Dekupl_3_5/sample_conditions.tsv"
+kmerICGC <- "/store/EQUIPES/SSFA/MEMBERS/thi-ngoc-ha.nguyen/Prostate_ICGC_148samples/Download/DEkupl_run_Relapse_Mask/Result_Dekupl_2_2/raw-counts.tsv.gz"
+totalKmersICGC <-"/store/EQUIPES/SSFA/MEMBERS/thi-ngoc-ha.nguyen/Prostate_ICGC_148samples/Download/DEkupl_run_Relapse_Mask/Result_kamrat_2_2/Contig_level/sum_counts.tsv"
+
+# Validation Stello data
+sampleStelloo <- "/store/EQUIPES/SSFA/MEMBERS/thi-ngoc-ha.nguyen/Prostate_data_validation/Stello_data/Result_Dekupl/sample_conditions.tsv"
+kmerStelloo <- "/store/EQUIPES/SSFA/MEMBERS/thi-ngoc-ha.nguyen/Prostate_data_validation/Stello_data/Result_Dekupl/limma_3_5/raw-counts-Stello-Relapse-3-3.tsv.gz"
+totalKmersStelloo <- "/store/EQUIPES/SSFA/MEMBERS/thi-ngoc-ha.nguyen/Prostate_data_validation/Stello_data/Result_kamrat_3_5/Contig_level/Total_kmer/sum_counts.tsv"
+
+NUM_RUNS=100
+nKeep = 500
+
+# Directory to store result
+dir.store <- paste0("Result_article/kmer_level/", "Elastic")
+dir.create(file.path(dir.store), showWarnings = FALSE, recursive = TRUE)
+############################################################################################
+normalizeContig <-function(validCountPath, libSizePath){
+  
+  # Normalization based on logCPM
+  countKmerICGC <- as.data.frame(fread(validCountPath, sep="\t", header = TRUE))
+  
+  rownames(countKmerICGC) <- countKmerICGC$tag
+  
+  countKmerICGC <- countKmerICGC[,-1]
+  
+  # Normalization based on logCPM
+  inforKmerICGC <- as.data.frame(fread(libSizePath, sep="\t", header = FALSE))
+  names(inforKmerICGC) <- c("Sample", "Total_kmers")
+  rownames(inforKmerICGC) <- inforKmerICGC$Sample
+  
+  #Sort libSize base on sample name
+  inforKmerICGC <- inforKmerICGC[colnames(countKmerICGC),]
+  libSizeICGC <- inforKmerICGC$Total_kmers
+  
+  # compute logBPM
+  logCpmICGC <- log(countKmerICGC/expandAsMatrix(libSizeICGC/1e+09, dim(countKmerICGC))+1)
+  
+  return (logCpmICGC)
+}
+
+#################
+evaluate_contig <- function(ctgMappDis, dataSigDis, sigContigDis, sigContigDisPath, dataValidPath, samplesConditionValidPath, modeValid){
+  
+  # Call the sript infer
+  cmdRun <- paste0("bash -c \"/store/EQUIPES/SSFA/MEMBERS/thi-ngoc-ha.nguyen/Haoliang_Find_Kmer/kmer-filter/kmerFilter -n -k 31 -f ",
+                   sigContigDisPath, " <(zcat ", dataValidPath ,") > ", dir.store,"/sig-contig-valid-", modeValid, ".tsv\"")
+  system(cmdRun)
+  
+  #Total kmers in this signature
+  totalKmerContig <- fromContig2Kmers(sigDiscovery = sigContigDis)
+  
+  # loading list of kmer found in ICGC dataset based on method
+  dataValidPath <- paste0(dir.store,"/sig-contig-valid-", modeValid, ".tsv")
+  
+  if(modeValid == "icgc"){
+    countKmerValid <- normalizeContig(validCountPath = dataValidPath, libSizePath = totalKmersICGC )
+  }else{
+    countKmerValid <- normalizeContig(validCountPath = dataValidPath, libSizePath = totalKmersStelloo )
+  }
+  
+  # Classify kmer belong to contig in ICGC dataset
+  kmerValid <- rownames(countKmerValid)
+  listClf <- list()
+  for(i in 1:length(totalKmerContig)){
+    
+    tmp <- c()
+    for(kmer in kmerValid){
+      
+      if(kmer%in%totalKmerContig[[i]])
+        tmp <- c(tmp, kmer)
+      
+    }
+    listClf[[i]]<- tmp
+  }
+  names(listClf) <- sigContigDis[1:length(listClf)]
+  
+  #Extract contigs present in validation dataset
+  listClf[sapply(listClf, is.null)] <- NULL
+  
+  # Calculate the count for each contig - based on mean count
+  dataValidRepre <- data.frame(matrix(ncol = dim(countKmerValid)[2], nrow = 0))
+  
+  for(i in 1:length(listClf)){
+    
+    frame <- countKmerValid[c(listClf[[i]]),]
+    repreCtg <- apply(data.matrix(frame),2, FUN = median)
+    dataValidRepre <- rbind(dataValidRepre, repreCtg)
+  }
+  rownames(dataValidRepre) <- names(listClf)
+  colnames(dataValidRepre) <- names(countKmerValid)
+  
+  strInfo <-paste("Contig signature include:", length(sigContigDis),"contigs that are created", length(unlist(totalKmerContig)),"kmer(s)\n",
+                  "There are: ",length(kmerValid),"kmers found in validation and belong to",length(listClf),"contigs")
+  
+  print(strInfo)
+  name.file <- paste0(dir.store, "/info-", modeValid, ".txt")
+  write.csv(strInfo, name.file)
+  
+  probesID <- rownames(dataValidRepre)
+  dataSignatureValid <- dataValidRepre
+  ########## processing of conditions ##############
+  #create sample condition for signature
+  samplesConditionValid<-as.data.frame(fread(samplesConditionValidPath,sep="\t", header= FALSE ,check.names=F))
+  names(samplesConditionValid)<-c("Sample","condition")
+  
+  for (i in 1:dim(samplesConditionValid)[1]){
+    if(samplesConditionValid$condition[i] == "no"){
+      samplesConditionValid$condition[i] = "No"
+    }
+    if(samplesConditionValid$condition[i] == "yes"){
+      samplesConditionValid$condition[i] = "Yes"
+    }
+  }
+  #Transpose
+  dataSignatureValid <- as.data.frame(t(dataSignatureValid))
+  
+  dataSignatureValid$Sample <- row.names(dataSignatureValid)
+  
+  # Mapping count table and condition in validation set
+  dataValid<-merge(dataSignatureValid,samplesConditionValid,
+                   by.x="Sample",
+                   by.y="Sample",
+                   all.x=TRUE,
+                   all.y=FALSE)
+  dataValid$condition <- factor(dataValid$condition)
+  
+  # dataframe of signature in ICGC dataset
+  dataSigValid <- dataValid[,c("condition", probesID)]
+  dataSave <- dataValid[,c("Sample", "condition", probesID)]
+  
+  print(paste("List of contig from TCGA found in", modeValid, "validation:"))
+  print(probesID)
+  ctgValidName <- c()
+  # Mapping contig to genome
+  for ( i in 1: length(probesID)){
+    
+    ctgValidName[i] = ctgMappDis[ctgMappDis$contig == probesID[i],]$UniqueName
+    print(ctgValidName[i])
+  }
+  
+  names(dataSigValid) <- c("condition", ctgValidName)
+  names(dataSave) <- c("Sample","condition", ctgValidName)
+  ########## signature performance ##############
+  #Combine data in TCGA and validation set
+  absentCtg <- setdiff(names(dataSigDis),names(dataSigValid))
+  print(paste(length(absentCtg), "contigs are not found in", modeValid, "valid set:"))
+  print(absentCtg)
+  if (length(absentCtg) != 0){
+    
+    for (contig in absentCtg){
+      dataSigValid$contig <- 0
+      names(dataSigValid)[length(names(dataSigValid))] <- contig
+    }
+  }
+  dataSigValid <- dataSigValid[,names(dataSigDis)]   
+  
+  
+  resSign <- takeDataReturnAUC(frameTrain = dataSigDis, frameTest = dataSigValid, absentContig = absentCtg, status = "relapse")
+  
+  return (list(resSign, dataSave))
+}
+
+################################
+pipeline <- function(topProbesPath, samplesConditionDisPath, numruns){
+  
+  # loading top  genes/contigs in TCGA discovery dataset based on approach level
+  countTopProbe <- as.data.frame(fread(topProbesPath, sep="\t", header = TRUE))
+    
+  countTopProbe <- data.frame(row.names = countTopProbe$feature, countTopProbe[,-c(1:4)], check.names=F)    
+
+  countTopProbe <- countTopProbe[1:nKeep,]
+
+  ########## processing of conditions ##############
+  samplesConditionDis<-as.data.frame(fread(samplesConditionDisPath,sep="\t", header= FALSE ,check.names=F))
+  names(samplesConditionDis)<-c("Sample","condition")
+  
+  #Transpose
+  countTopProbe <- as.data.frame(t(countTopProbe))
+  
+  #Change the double value in right format
+  for (i in 1:dim(countTopProbe)[2]){
+    countTopProbe[,i] <- as.double(as.character(countTopProbe[,i]))
+  }
+
+  countTopProbe$Sample <- row.names(countTopProbe)
+  
+  # Mapping count table and condition in TCGA
+  dataTCGA<-merge(countTopProbe,samplesConditionDis, 
+                  by.x="Sample",
+                  by.y="Sample",
+                  all.x=TRUE,
+                  all.y=FALSE)
+  dataTCGA$condition <- factor(dataTCGA$condition)
+  
+  ########## processing of feature selection ##############
+  
+  # Finding the signature in TCGA discovery dataset
+  dataModel <-dataTCGA[,-which(names(dataTCGA) %in% c("condition","Sample"))]
+  dataModel$condition <-factor(dataTCGA$condition)
+  
+  # Set threshold for Lasso logistic regression
+  th_lasso = 0.5
+  
+  sigTCGA <- extractSignatureStb(dataModel, thres = th_lasso)
+    
+  print("List of contig in the signature:")
+  print(sigTCGA)
+  
+  # Mapping contig to genenome
+  # Create fa file
+  insertStr <- paste0(">ctg_",seq(1:length(sigTCGA)))
+  preFA <- as.vector(t(cbind(insertStr, matrix(sigTCGA, length(sigTCGA), byrow=T))))
+  
+  name.file <- paste0(dir.store, "/sig-contig-tcga.fa")
+  write.table(preFA, file = name.file, quote = FALSE, row.names = FALSE, col.names = FALSE)
+  
+  # Table assig ctg -> contig
+  assignCtg <- as.data.frame(cbind(paste0("ctg_",seq(1:length(sigTCGA))), sigTCGA))
+  names(assignCtg) <- c("Alias", "contig")    
+  
+  # Call blast command
+  cmdRun <- paste0("bash -c \"blastn -db /store/EQUIPES/SSFA/Index/Gencode/gencode.v34.transcripts.fa -query ",
+                   dir.store, "/sig-contig-tcga.fa -evalue 1e-3 -max_target_seqs 1 -outfmt 6 -max_hsps 1 -word_size 10 ",
+                   #   '|awk -F"\t"', "'{print $1" , '"\t"', "$2}'", '|awk -F"|"', " '{print $1",' "\t" ', "$6}'",'|awk -F"\t"'," '{print $1",' "\t"', " $3}'",
+                   " > ", dir.store,"/sig-contig-tcga-geneName.tsv\"")
+  system(cmdRun)      
+  
+  #Open file geneName
+  contigName <- as.data.frame(fread(input = paste0(dir.store,"/sig-contig-tcga-geneName.tsv"), sep = "\t", header = FALSE))
+  contigName <- contigName[,c(1,2)]
+  names(contigName) <- c("Alias", "Name")
+  
+  for (i in 1:dim(contigName)[1]){
+    
+    contigName$Name[[i]] = unlist(strsplit(contigName$Name[[i]],"\\|"))[6]
+  }
+  
+  # Mapping 
+  mapAlliasName <- merge(assignCtg, contigName,
+                         by.x = "Alias",
+                         by.y = "Alias",
+                         all.x = TRUE,
+                         all.y = FALSE)
+  uniqueGene <- c()
+  for (i in 1:dim(mapAlliasName)[1]){
+    if(is.na(mapAlliasName$Name[i])){
+      
+      uniqueGene[i] = mapAlliasName$Alias[i]
+      
+    }else{
+      if(mapAlliasName$Name[i] %in% uniqueGene){
+        uniqueGene[i] = paste0(mapAlliasName$Name[i], ".v",i)
+      }else{
+        uniqueGene[i] = mapAlliasName$Name[i]
+      }
+    }
+  }
+
+  mapAlliasName$UniqueName = uniqueGene
+  contigMapp <- mapAlliasName[, c("contig", "UniqueName")]
+  
+  #Save to file
+  name.file <- paste0(dir.store, "/sig-contig-tcga.tsv")
+  write.table(mapAlliasName[, c("contig")], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)   
+  
+  name.file <- paste0(dir.store, "/sig-contig-genemap-tcga.tsv")
+  write.table(contigMapp, file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)       
+
+  # dataframe of signature in TCGA dataset
+  dataSigTCGA <- dataTCGA[,c("condition", sigTCGA)]
+
+  #Order the geneName base on the contig sequence
+  # Transpose contigMapp with colname is contig
+  contigName <- as.data.frame(t(contigMapp[,-c(1)]))
+  colnames(contigName) <- contigMapp$contig
+  contigName = as.data.frame(contigName[, sigTCGA])
+  
+  ctgNameDis = unname(unlist(contigName[1,]))
+  
+  print("List of contig mapping to genome: ")
+  print(ctgNameDis)
+  
+  name.file <- paste0(dir.store, "/data-sig-contig-tcga.tsv")
+  dataSaveTCGA <- dataTCGA[,c("Sample","condition",sigTCGA)]
+  colnames(dataSaveTCGA) <- c("Sample", "condition", ctgNameDis)
+  write.table(dataSaveTCGA, file = name.file, sep = "\t", quote = FALSE, row.names = FALSE)
+  names(dataSigTCGA) <- c("condition", ctgNameDis)
+  ########## finding signature in validation set ##############
+
+  #Valid in ICGC
+  resSigICGC <- evaluate_contig(ctgMappDis = contigMapp, dataSigDis = dataSigTCGA, sigContigDis = sigTCGA, sigContigDisPath = paste0(dir.store, "/sig-contig-tcga.tsv"), 
+                                dataValidPath = kmerICGC, samplesConditionValidPath = sampleICGC, modeValid = "icgc")
+  
+  # Store important variable 
+  name.file <- paste0(dir.store, "/import-sig-contig-icgc.tsv")
+  importVar <- as.data.frame(resSigICGC[[1]][[4]]$importance)
+  importVar$Gene <- rownames(resSigICGC[[1]][[4]]$importance)
+  names(importVar) <- c("Overall", "Gene")
+  dataStore <- importVar[round(order(importVar$Overall, decreasing = T),4),]
+  print(dataStore[, c("Gene","Overall")])
+  write.table(dataStore[, c("Gene", "Overall")], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+  
+  #Store data frame valid
+  name.file <- paste0(dir.store, "/data-sig-contig-icgc.tsv")
+  write.table(resSigICGC[[2]], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+  
+  # Valid in Stelloo
+  resSigStelloo <- evaluate_contig(ctgMappDis = contigMapp, dataSigDis = dataSigTCGA,sigContigDis = sigTCGA, sigContigDisPath = paste0(dir.store, "/sig-contig-tcga.tsv"), 
+                                   dataValidPath = kmerStelloo, samplesConditionValidPath = sampleStelloo, modeValid = "stello")
+  
+
+  # Store important variable 
+  name.file <- paste0(dir.store, "/import-sig-contig-stelloo.tsv")
+  importVar <- as.data.frame(resSigStelloo[[1]][[4]]$importance)
+  importVar$Gene <- rownames(resSigStelloo[[1]][[4]]$importance)
+  names(importVar) <- c("Overall", "Gene")
+  dataStore <- importVar[round(order(importVar$Overall, decreasing = T),4),]
+  print(dataStore[, c("Gene","Overall")])
+  write.table(dataStore[, c("Gene", "Overall")], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+  
+  #Store data frame valid
+  name.file <- paste0(dir.store, "/data-sig-contig-stelloo.tsv")
+  write.table(resSigStelloo[[2]], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+  
+  finalTab <- rbind(resSigICGC[[1]][[3]], resSigStelloo[[1]][[3]], resSigICGC[[1]][[1]], resSigICGC[[1]][[2]])
+  rownames(finalTab) <- c("AUC signature contig in ICGC", "AUC signature contig in Stelloo", 
+			  "Mean AUC cross-valid (TCGA)", "SD AUC cross-valid (TCGA)")
+  print(finalTab)
+  
+  name.file <- paste0(dir.store, "/auc-sig-cotig.tsv")
+  write.table(finalTab, file = name.file, quote = FALSE, row.name = TRUE, col.names = FALSE)
+  
+}
+#################################
+#Run the script to infer
+resContig <- pipeline(topProbesPath = topContig, samplesConditionDisPath = sampleTCGA, numruns = NUM_RUNS)
