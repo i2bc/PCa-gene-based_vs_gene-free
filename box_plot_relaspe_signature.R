06b4df7d55a17666b085f183f29396da241eadd6
hantnTLU <54317348+hantnTLU@users.noreply.github.com>
2020-08-13 20:40:43 +0200

    Update box_plot_relaspe_signature.R

--- box_plot_relaspe_signature.R
+++ box_plot_relaspe_signature.R
@@ -11,10 +11,9 @@ set.seed(12678)
 # Load libraries
 ################
 library(data.table)
+library(ggplot2)
 ################
-# Load list of functions 
-################
-source("useful_functions.R")
+
 #######################################################################
 ## scripts
 #######################################################################
@@ -42,7 +41,70 @@ dataContigIcgc <- as.data.frame(fread(contig_valid_icgc, sep="\t", header = TRUE
 dataContigStelloo <- as.data.frame(fread(contig_valid_stelloo, sep="\t", header = TRUE))
 
 ###########################################
-
+boxPlot <- function(dataPlot, modeLevel, dir.name, status){
+  # Plot box plot 
+  #
+  # Args:
+  #   dataPlot: Data frame containes expression of selected probes
+  #   modeLevel : String indicates gene-level or kmer-level
+  #   dir.name : Path storing
+  #   status : String indicates with database will be use (tcga/icgc/stelloo)
+  # Results:
+  #   Figure for boxplot
+  
+  #Plot boxplot for the count of signature
+  white_background<-theme(axis.line = element_line(colour = "black"),
+                          panel.grid.major = element_blank(),
+                          panel.grid.minor = element_blank(),
+                          panel.background = element_blank())
+  #Unlog the dataPlot into CPM/CPB expression
+  unlog <- exp(dataPlot[-c(1,2)])
+  dataNormalized <- cbind(dataPlot[c(1,2)], unlog)
+  
+  nameProbe <- names(dataNormalized[3:length(names(dataNormalized))])
+  my.data <- melt(dataNormalized,measure.vars=c(names(dataNormalized[3:length(names(dataNormalized))])))
+  my.data$value <- as.numeric(my.data$value)
+  if(modeLevel == "contig"){xlabel = "Contigs"}
+  if(modeLevel == "gene"){xlabel = "Genes"}
+  
+  boxplots_frame<-data.frame()
+  
+  for(i in 1: length(nameProbe)){
+    
+    HRvalues <- my.data[which(my.data[,"condition"]=="Yes" & my.data$variable == nameProbe[i]),]$value
+    
+    meanHR <- mean(HRvalues)
+    
+    LRvalues <- my.data[which(my.data[,"condition"]=="No" & my.data$variable == nameProbe[i]),]$value
+    
+    meanLR<- mean(LRvalues)
+    
+    logFC<- log(meanHR/ meanLR)
+    
+    wilcoxon_pvalue<-as.numeric(format(wilcox.test(LRvalues,HRvalues)$p.value,scientific=T,digits=4,quote=F))
+    
+    boxplots_frame<-rbind(boxplots_frame,data.frame(probes=nameProbe[i],mean_NonRelapse = round(meanLR, digits=2),mean_Relapse = round(meanHR, digits=2),logFC=round(logFC, digits=2),wilcoxon_pvalue=wilcoxon_pvalue)) 
+  }
+  
+  #The order of contig based on logFC decreasing
+  ctgLogFC <- nameProbe[order(boxplots_frame$logFC, decreasing = TRUE)]
+  frameLogFC <- boxplots_frame[match(ctgLogFC,boxplots_frame$probes),]
+  # Save
+  name.file <- paste0(dir.name, "logFC-sig-", modeLevel, "-", status,".tsv")
+  
+  write.table(frameLogFC, file = name.file, sep = "\t", quote = FALSE, row.names = FALSE)
+  
+  # Reoder the dataframe
+  dataPlot$condition = factor(dataPlot$condition, levels = c("No", "Yes"))
+  data.plot <- melt(dataPlot,measure.vars= ctgLogFC)
+  boxPlot<-ggplot(data.plot, aes(x=variable,y=value,fill=condition))+ theme_bw() + geom_boxplot(outlier.size =0.5)+
+    ylab("log(normalized expression)")+xlab(xlabel)+ggtitle(paste("Distribution of probes expression\n","nb probes = ",	length(unique(my.data$variable)),"\nnb samples = ",length(unique(my.data$Sample)),
+         " (NO = ",length(unique(my.data[which(my.data$condition=="No"),]$Sample))," ; YES = ",length(unique(my.data[which(my.data$condition=="Yes"),]$Sample)),")",sep=""))+
+    theme(axis.text.x=element_text(color="black",size=12,angle=45,hjust=1),axis.text.y = element_text(color="black",size=14),plot.title = element_text(hjust = 0.5,size=16),
+          axis.title=element_text(size=16,face="bold"),legend.text=element_text(size=14),legend.title=element_text(size=16))+
+    scale_fill_manual(values=c("No"="cornflowerblue","Yes"="red"),name="conditions") + white_background
+}
+###########################################
 # Box plot for signature in TCGA
 boxPlotGeneDis <- boxPlot(dataPlot = dataGeneDis, modeLevel = "gene" , dir.name = dir.storeGene, status = "tcga")
 png(filename = paste0(dir.storeGene, "box-plot-sig-gene-tcga.png"), width = 3200, height = 1600, units = "px", res = 300)
