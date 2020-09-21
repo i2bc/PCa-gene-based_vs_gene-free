49c3d076a31a0c82f5eef60f85f59f09791d6125
Haoliang XUE <haoliang.xue@i2bc.paris-saclay.fr>
2020-09-21 16:26:55 +0200

    change in merge_norm_reduce from -N 2000 to -N 500

--- KaMRaT/merge_norm_reduce.bash
+++ KaMRaT/merge_norm_reduce.bash
@@ -10,4 +10,4 @@ source /home/haoliang.xue/.bashrc       # for export LD_LIBRARY_PATH with mlpack
 
 /store/EQUIPES/SSFA/haoliang-shared/KaMRaT_for_Paper_Ha/PCa-gene-based_vs_gene-free/KaMRaT/bin/kamratNorm -b B -d $smp_condition_path -T log $out_dir/raw-counts-merge-TCGA-LRHR-3-10.tsv > $out_dir/raw-counts-merge-norm-TCGA-LRHR-3-10.tsv
 
-/store/EQUIPES/SSFA/haoliang-shared/KaMRaT_for_Paper_Ha/PCa-gene-based_vs_gene-free/KaMRaT/bin/kamratReduce -d $smp_condition_path -N 2000 -m nb:5 $out_dir/raw-counts-merge-norm-TCGA-LRHR-3-10.tsv > $out_dir/top2k_contig_merge_norm.nb5.out
+/store/EQUIPES/SSFA/haoliang-shared/KaMRaT_for_Paper_Ha/PCa-gene-based_vs_gene-free/KaMRaT/bin/kamratReduce -d $smp_condition_path -N 500 -m nb:5 $out_dir/raw-counts-merge-norm-TCGA-LRHR-3-10.tsv > $out_dir/top2k_contig_merge_norm.nb5.out
