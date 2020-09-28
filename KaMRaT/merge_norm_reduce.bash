028e25e5f7c9d7af7bfc1f69bc50de2d3f49881a
haoliang.xue <haoliang.xue@i2bc.paris-saclay.fr>
2020-09-28 21:19:54 +0200

    merge_norm_reduce: add temporary directory as out_dir

--- KaMRaT/merge_norm_reduce.bash
+++ KaMRaT/merge_norm_reduce.bash
@@ -4,7 +4,7 @@ raw_count_path=$1
 smp_condition_path=$2
 out_dir=$3
 
-/store/EQUIPES/SSFA/haoliang-shared/KaMRaT_for_Paper_Ha/PCa-gene-based_vs_gene-free/KaMRaT/bin/kamratMerge -i mac -q mean -n <(zcat $raw_count_path) > $out_dir/raw-counts-merge-TCGA-LRHR-3-10.tsv
+/store/EQUIPES/SSFA/haoliang-shared/KaMRaT_for_Paper_Ha/PCa-gene-based_vs_gene-free/KaMRaT/bin/kamratMerge -i mac -q mean -n -t $out_dir <(zcat $raw_count_path) > $out_dir/raw-counts-merge-TCGA-LRHR-3-10.tsv
 
 /store/EQUIPES/SSFA/haoliang-shared/KaMRaT_for_Paper_Ha/PCa-gene-based_vs_gene-free/KaMRaT/bin/kamratNorm -b B -d $smp_condition_path -T log $out_dir/raw-counts-merge-TCGA-LRHR-3-10.tsv > $out_dir/raw-counts-merge-norm-TCGA-LRHR-3-10.tsv
 
