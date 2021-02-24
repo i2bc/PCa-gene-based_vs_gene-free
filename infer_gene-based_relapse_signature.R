7d2dcd2185362b6cf309402798c71dad1a3e3d3b
hantnTLU <54317348+hantnTLU@users.noreply.github.com>
2021-02-24 10:54:14 +0100

    Update infer_gene-based_relapse_signature.R

--- infer_gene-based_relapse_signature.R
+++ infer_gene-based_relapse_signature.R
@@ -1,6 +1,6 @@
 ################
 # Author : Thi Ngoc Ha Nguyen
-# Date   : 05/07/2020
+# Date   : 04/10/2020
 # Email  : thi-ngoc-ha.nguyen@i2bc.paris-saclay.fr
 ################
 
@@ -27,7 +27,7 @@ sampleTCGA <- paste0(dir.discovery, "sample_conditions.tsv")
 
 # Validation ICGC data
 dir.validation <- "Data_validation/Relapse/"
-		     
+
 sampleICGC <- paste0(dir.validation, "sample_cond_icgc.tsv")
 geneICGC <- paste0(dir.validation, "gene-norm-ICGC-Relapse-2-2.tsv")
 
@@ -86,9 +86,11 @@ evaluate_gene <- function( sigGeneDis, dataSigDis, dataValidPath, samplesConditi
   
   names(dataSigValid) <- c("condition",signature.save)
   
-  resSign <- takeDataReturnAUC(frameTrain = dataSigDis, frameTest = dataSigValid, absentContig = c(), status = "relapse")  
+  resSignROC <- takeDataReturnAUC(frameTrain = dataSigDis, frameTest = dataSigValid, absentContig = c(), status = "relapse")  
   
-  return(list(resSign, dataSaveValid))
+  resSignPR <- takeDataReturnPR(frameTrain = dataSigDis, frameTest = dataSigValid, absentContig = c(), status = "relapse")  
+  
+  return(list(resSignROC, dataSaveValid, resSignPR))
 }
 
 #################
@@ -97,7 +99,7 @@ pipeline <- function(topProbesPath, samplesConditionDisPath, numruns){
   # loading top  genes/contigs in TCGA discovery dataset based on approach level
   countTopProbe <- as.data.frame(fread(topProbesPath, sep="\t", header = TRUE))
   countTopProbe <- data.frame(row.names = countTopProbe$feature, countTopProbe[,-c(1,2)], check.names=F)
-
+  
   ########## processing of conditions ##############
   samplesConditionDis<-as.data.frame(fread(samplesConditionDisPath,sep="\t", header= FALSE ,check.names=F))
   names(samplesConditionDis)<-c("Sample","condition")
@@ -125,7 +127,7 @@ pipeline <- function(topProbesPath, samplesConditionDisPath, numruns){
   th_lasso = 0.5
   
   sigTCGA <- extractSignatureStb(dataModel, thres = th_lasso)
-
+  
   # From gene symbol to gene name
   signature.idsym <- fromGeneIDtakeGenName(sigTCGA)
   signature.save <- signature.idsym$SYMBOL
@@ -136,58 +138,94 @@ pipeline <- function(topProbesPath, samplesConditionDisPath, numruns){
   name.file <- paste0(dir.store, "/sig-gene-tcga.txt")
   
   write.table(signature.idsym, file = name.file, quote = FALSE, row.names = FALSE, col.names = FALSE)
-
+  
   # dataframe of signature in TCGA dataset
   dataSigTCGA <- dataTCGA[,c("condition", sigTCGA)]
   names(dataSigTCGA) <- c("condition", signature.save)
-
+  
   name.file <- paste0(dir.store, "/data-sig-gene-tcga.tsv")
   dataSaveTCGA <- dataTCGA[,c("Sample","condition",sigTCGA)]
   colnames(dataSaveTCGA) <- c("Sample", "condition", signature.save)
   write.table(dataSaveTCGA, file = name.file, sep = "\t", quote = FALSE, row.names = FALSE)
-
+  
   ########## finding signature in validation set ##############
-
+  
   #Valid in ICGC
   resSigICGC <- evaluate_gene(dataSigDis = dataSigTCGA, sigGeneDis = sigTCGA, 
                               dataValidPath = geneICGC, samplesConditionValidPath = sampleICGC) 
-
-  # Store important variable 
-  name.file <- paste0(dir.store, "/import-sig-gene-icgc.tsv")
-  importVar <- as.data.frame(resSigICGC[[1]][[4]]$importance)
-  importVar$Gene <- rownames(resSigICGC[[1]][[4]]$importance)
-  names(importVar) <- c("Overall", "Gene")
-  dataStore <- importVar[round(order(importVar$Overall, decreasing = T),4),]
-  print(dataStore[, c("Gene","Overall")])
-  write.table(dataStore[, c("Gene", "Overall")], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
-
+  
   #Store data frame valid
   name.file <- paste0(dir.store, "/data-sig-gene-icgc.tsv")
   write.table(resSigICGC[[2]], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+  finalTab <- rbind(resSigICGC[[1]][[1]], resSigICGC[[1]][[2]], resSigICGC[[1]][[3]], resSigICGC[[1]][[4]],
+                    resSigICGC[[1]][[5]], resSigICGC[[1]][[6]], resSigICGC[[1]][[7]], resSigICGC[[1]][[8]],
+                    resSigICGC[[1]][[9]], resSigICGC[[1]][[10]], resSigICGC[[1]][[11]], resSigICGC[[1]][[12]])
+  rownames(finalTab) <- c( 
+    "Mean ROC_AUC cv (TCGA)", "SD ROC_AUC cv (TCGA)", 
+    "Mean ROC_AUC down cv (TCGA)", "SD ROC_AUC down cv (TCGA)",
+    "Mean ROCAUC up cv (TCGA)", "SD ROC_AUC up cv (TCGA)",
+    "ROC_AUC sig gene in ICGC", "PrAUC sig gene in ICGC",
+    "ROC_AUC down sig gene in ICGC", "PrAUC down sig gene in ICGC",
+    "ROC_AUC up sig gene in ICGC", "PrAUC up sig gene in ICGC")
+  print(finalTab)
+
+  name.file <- paste0(dir.store, "/Roc_auc-sig-gene-icgc.tsv")
+  write.table(finalTab, file = name.file, quote = FALSE, row.name = TRUE, col.names = FALSE) 
   
+  finalTab <- rbind(resSigICGC[[3]][[1]], resSigICGC[[3]][[2]], resSigICGC[[3]][[3]], resSigICGC[[3]][[4]],
+                    resSigICGC[[3]][[5]], resSigICGC[[3]][[6]], resSigICGC[[3]][[7]], resSigICGC[[3]][[8]],
+                    resSigICGC[[3]][[9]], resSigICGC[[3]][[10]], resSigICGC[[3]][[11]], resSigICGC[[3]][[12]])
+  rownames(finalTab) <- c( 
+    "Mean PR_AUC cv (TCGA)", "SD PR_AUC cv (TCGA)", 
+    "Mean PR_AUC down cv (TCGA)", "SD PR_AUC down cv (TCGA)",
+    "Mean PR_AUC up cv (TCGA)", "SD PR_AUC up cv (TCGA)",
+    "ROC_AUC sig gene in ICGC", "PrAUC sig gene in ICGC",
+    "ROC_AUC down sig gene in ICGC", "PrAUC down sig gene in ICGC",
+    "ROC_AUC up sig gene in ICGC", "PrAUC up sig gene in ICGC")
+  print(finalTab)  
+  
+  name.file <- paste0(dir.store, "/Pr_auc-sig-gene-icgc.tsv")
+  write.table(finalTab, file = name.file, quote = FALSE, row.name = TRUE, col.names = FALSE)   
+    
   #Valid in Stelloo
   resSigStelloo <- evaluate_gene(dataSigDis = dataSigTCGA, sigGeneDis = sigTCGA, 
                                  dataValidPath = geneStelloo, samplesConditionValidPath = sampleStelloo)
-  # Store important variable 
-  name.file <- paste0(dir.store, "/import-sig-gene-stelloo.tsv")
-  importVar <- as.data.frame(resSigStelloo[[1]][[4]]$importance)
-  importVar$Gene <- rownames(resSigStelloo[[1]][[4]]$importance)
-  names(importVar) <- c("Overall", "Gene")
-  dataStore <- importVar[round(order(importVar$Overall, decreasing = T),4),]
-  print(dataStore[, c("Gene","Overall")])
-  write.table(dataStore[, c("Gene", "Overall")], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
-  
+ 
   #Store data frame valid
   name.file <- paste0(dir.store, "/data-sig-gene-stelloo.tsv")
   write.table(resSigStelloo[[2]], file = name.file, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
   
-  finalTab <- rbind(resSigICGC[[1]][[3]], resSigStelloo[[1]][[3]], resSigICGC[[1]][[1]], resSigICGC[[1]][[2]])
-  rownames(finalTab) <- c("AUC signature gene in ICGC", "AUC signature gene in Stelloo", 
-						  "Mean AUC cross-valid (TCGA)", "SD AUC cross-valid (TCGA)")
+  finalTab <- rbind(resSigStelloo[[1]][[1]], resSigStelloo[[1]][[2]], resSigStelloo[[1]][[3]], resSigStelloo[[1]][[4]],
+                    resSigStelloo[[1]][[5]], resSigStelloo[[1]][[6]], resSigStelloo[[1]][[7]], resSigStelloo[[1]][[8]],
+                    resSigStelloo[[1]][[9]], resSigStelloo[[1]][[10]], resSigStelloo[[1]][[11]], resSigStelloo[[1]][[12]])
+  rownames(finalTab) <- c( 
+    "Mean ROC_AUC cv (TCGA)", "SD ROC_AUC cv (TCGA)", 
+    "Mean ROC_AUC down cv (TCGA)", "SD ROC_AUC down cv (TCGA)",
+    "Mean ROCAUC up cv (TCGA)", "SD ROC_AUC up cv (TCGA)",
+    "ROC_AUC sig gene in Stelloo", "PrAUC sig gene in Stelloo",
+    "ROC_AUC down sig gene in Stelloo", "PrAUC down sig gene in Stelloo",
+    "ROC_AUC up sig gene in Stelloo", "PrAUC up sig gene in Stelloo")
   print(finalTab)
   
-  name.file <- paste0(dir.store, "/auc-sig-gene.tsv")
-  write.table(finalTab, file = name.file, quote = FALSE, row.name = TRUE, col.names = FALSE)  
+  name.file <- paste0(dir.store, "/Roc_auc-sig-gene-stelloo.tsv")
+  write.table(finalTab, file = name.file, quote = FALSE, row.name = TRUE, col.names = FALSE) 
+  
+  finalTab <- rbind(resSigStelloo[[3]][[1]], resSigStelloo[[3]][[2]], resSigStelloo[[3]][[3]], resSigStelloo[[3]][[4]],
+                    resSigStelloo[[3]][[5]], resSigStelloo[[3]][[6]], resSigStelloo[[3]][[7]], resSigStelloo[[3]][[8]],
+                    resSigStelloo[[3]][[9]], resSigStelloo[[3]][[10]], resSigStelloo[[3]][[11]], resSigStelloo[[3]][[12]])
+  rownames(finalTab) <- c( 
+    "Mean PR_AUC cv (TCGA)", "SD PR_AUC cv (TCGA)", 
+    "Mean PR_AUC down cv (TCGA)", "SD PR_AUC down cv (TCGA)",
+    "Mean PR_AUC up cv (TCGA)", "SD PR_AUC up cv (TCGA)",
+    "ROC_AUC sig gene in Stelloo", "PrAUC sig contig in Stelloo",
+    "ROC_AUC down sig gene in Stelloo", "PrAUC down sig gene in Stelloo",
+    "ROC_AUC up sig gene in Stelloo", "PrAUC up sig gene in Stelloo")
+  print(finalTab)  
+  
+  name.file <- paste0(dir.store, "/Pr_auc-sig-gene-stelloo.tsv")
+  write.table(finalTab, file = name.file, quote = FALSE, row.name = TRUE, col.names = FALSE)   
+  
 }
 #################################
 #Run the script to infer
